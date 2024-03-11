d39c5eece050f50efbef3a36b861d6ff08952dae
dorot22 <dorot22@hotmail.com>
2024-03-11 10:23:19 +0100

    mise au point affichage image focus

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ imageContainers.forEach((element) => {
         document.querySelector('#presentation').style.filter = 'blur(5px)';
         imageFocus.style.filter = 'none';
         imageFocus.style.width = '50%';
-        imageFocus.style.margin = '2% auto auto auto';
+        imageFocus.style.margin = 'auto';
         imageFocus.style.padding = '1%';
         imageFocus.style.backgroundColor = '#e3cd8bad';
         photoDescriptionFocus.style.display = 'block';
@@ -36,7 +36,9 @@ imageContainers.forEach((element) => {
 
 document.addEventListener('click', (event) => {
     if (imageFocus != '') {
+        console.log(event.target)
         if (!Array.from(imageContainers).some(container => container.contains(event.target)) && !rightArrow.contains(event.target) && !leftArrow.contains(event.target)) {
+            console.log(41)
             isFocus = false;
             body.style.backgroundColor = 'transparent';
             divImageFocus.style.display = "none";
@@ -49,6 +51,60 @@ document.addEventListener('click', (event) => {
             imageFocus = '';
         }
     }
+});
+
+rightArrow.addEventListener('click', (event) => {
+    scrollPhoto('right')
+});
+
+leftArrow.addEventListener('click', (event) => {
+    scrollPhoto('left')
+});
+
+
+document.addEventListener("keydown", function (event) {
+    if(focus){
+        if (event.key === "ArrowRight") {
+            scrollPhoto('right')
+        }
+        if (event.key === "ArrowLeft") {
+            scrollPhoto('left')
+        };
+    }
 
 });
 
+function scrollPhoto(direction) {
+    var allPhotos = imageRef.parentElement.querySelectorAll('img');
+    var indice = Array.from(allPhotos).indexOf(imageRef)
+    var imageContainersLength = allPhotos.length
+    if (direction == 'right') {
+        if (indice + 1 == imageContainersLength) {
+            imageRef = allPhotos[0]
+        } else {
+            imageRef = allPhotos[indice + 1]
+        }
+    } else if (direction == 'left') {
+        if (indice - 1 == -1) {
+            imageRef = allPhotos[imageContainersLength - 1]
+        } else {
+            imageRef = allPhotos[indice - 1]
+        }
+    }
+    console.log(imageRef)
+    // var newPhotoDescriptionFocus = imageRef.nextElementSibling.cloneNode(true);
+    // newPhotoDescriptionFocus.style.display = 'block';
+    var newimageFocus = imageRef.cloneNode();
+    console.log(newimageFocus)
+    newimageFocus.style.width = '50%';
+    newimageFocus.style.margin = 'auto';
+    newimageFocus.style.padding = '1%';
+    newimageFocus.style.backgroundColor = '#e3cd8bad';
+    newimageFocus.style.display = 'block';
+    newimageFocus.style.clipPath = 'none';
+    pictureAndDescriptionFocus.replaceChild(newimageFocus, imageFocus);
+    // pictureAndDescriptionFocus.replaceChild(newPhotoDescriptionFocus, photoDescriptionFocus);
+    imageFocus = newimageFocus
+    // photoDescriptionFocus = newPhotoDescriptionFocus
+
+}
